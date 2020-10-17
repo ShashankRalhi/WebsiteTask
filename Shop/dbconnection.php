423aa8a7c26d0f99ec17883992b79cb68341d677
ShashankRalhi <shashankralhi1999@gmail.com>
2020-10-17 19:24:47 +0530

    website

---
+++ Shop/dbconnection.php
@@ -0,0 +1,18 @@
+<?php 
+
+	$dbhost = "localhost";
+	$dbuser = "root";
+	$dbpass = "";
+	$dbname = "website"; 
+
+	// Create connection
+	$conn = new mysqli($dbhost, $dbuser, $dbpass , $dbname);
+
+	// Check connection
+	if ($conn->connect_error) 
+	{
+	  die("Connection failed: " . $conn->connect_error);
+	}
+		//echo "Connected successfully";
+
+?>
